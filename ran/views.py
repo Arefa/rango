99839c660e585b76a28594a26b9b3ea72906922e
Arefa <longlan.cqu@gmail.com>
2015-12-14 15:45:16 +0800

    8.2

--- ran/views.py
+++ ran/views.py
@@ -1,10 +1,15 @@
 # -*-coding: utf-8 -*-
 # from django.http import HttpResponse
 from django.shortcuts import render
+from ran.models import Category, Page
+from ran.forms import CategoryForm
 
 
 def index(request):
-    context_dict = {'boldmessage': '这里是boldmessage'}
+    category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
+    context_dict = {'categories': category_list, 'pages': page_list}
+    # context_dict = {'boldmessage': '这里是boldmessage'}
     return render(request, 'ran/index.html', context_dict)
     # return HttpResponse('欢迎啊!<br/><a href="/ran/about">关于</a>')
 
@@ -13,3 +18,30 @@ def about(request):
     context_dict = {'about': '这里是about'}
     return render(request, 'ran/about.html', context_dict)
     # return HttpResponse('关于页面<br/><a href="/ran/">主页</a>')
+
+
+def category(request, category_name_slug):
+    context_dict = {}
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        context_dict['category_name'] = category.name
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        pass
+    return render(request, 'ran/category.html', context_dict)
+
+
+def add_category(request):
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print form.errors
+    else:
+        form = CategoryForm()
+
+    return render(request, 'ran/add_category.html', {'form': form})
