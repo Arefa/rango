9065829de8d01c99bde42d7f155beff61e6d7bcc
Arefa <longlan.cqu@gmail.com>
2015-12-14 23:57:46 +0800

    zhuce

--- ran/forms.py
+++ ran/forms.py
@@ -1,6 +1,8 @@
 # -*-coding: utf-8 -*-
 from django import forms
-from ran.models import Page, Category
+from django.contrib.auth.models import User
+
+from ran.models import Page, Category, UserProfile
 
 
 class CategoryForm(forms.ModelForm):
@@ -32,3 +34,17 @@ class PageForm(forms.ModelForm):
             cleaned_data['url'] = url
 
         return cleaned_data
+
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('website', 'picture')
